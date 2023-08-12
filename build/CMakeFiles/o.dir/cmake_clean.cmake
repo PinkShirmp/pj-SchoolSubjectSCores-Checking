c2ab477c8f6d3e8ff39c6f963395f3d9d820ff88
PinkShirmp <nminhtue094@gmail.com>
2023-08-12 22:14:10 +0700

    hi

---
+++ build/CMakeFiles/o.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/o.dir/src/main.cpp.o"
+  "CMakeFiles/o.dir/src/main.cpp.o.d"
+  "o"
+  "o.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/external/matplotlib/CMakeFiles/basic.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/tue/Desktop/code/Leaning-progress-superviosr/external/matplotlib/examples/basic.cpp" "external/matplotlib/CMakeFiles/basic.dir/examples/basic.cpp.o" "gcc" "external/matplotlib/CMakeFiles/basic.dir/examples/basic.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
